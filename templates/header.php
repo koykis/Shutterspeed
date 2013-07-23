640569b3e241d13314ef95bd7604ebe7444f90ab
Koykis <koykis@gmail.com>
2013-07-23 15:02:44 +0300

    Header file with all the basic includes.
    Can be edited to include more javascript or css files. Also contains the initialization of the facebook javascript API.

---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<?php  
+	ptRedirect('http://www.facebook.com/page/app_id');
+?>
+<!DOCTYPE html>
+<html>
+
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title></title>	
+	<link rel="stylesheet" href="css/style.css"/>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+  <script type="text/javascript" src="js/jsdk.js"></script>
+	<script type="text/javascript" src="js/jCarouselite.js"></script>
+	<script type="text/javascript" src="js/mCustomScrollbar.js"></script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php include 'scripts/analytics.php'; ?>
+</head>
+
+<body>
+	<div id="fb-root"></div>
+	<script src="//connect.facebook.net/en_US/all.js"></script>
+	
+	<div id="loading">
+		<center><img src="img/loading.gif" alt="Loading..." width="150" /></center>
+	</div>
