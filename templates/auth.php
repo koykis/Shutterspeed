fac13e3d5ef24b1a82ae16d1bae278deaa97070f
Koykis <koykis@gmail.com>
2013-07-23 15:24:41 +0300

    Authorization page
    A page with a button that calls a function of the facebook API, calling the user to authorize the application for access to his/her profile.

---
+++ templates/auth.php
@@ -0,0 +1,8 @@
+
+<div id="authwrapper">
+
+  <div id="authbtn">
+		<div id="login" onclick="login()"><img src="img/buttons/allow-button.png" alt="Πάτα εδώ για να συνεχίσεις" /></div>
+	</div>
+
+</div>
