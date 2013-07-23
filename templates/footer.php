301a92dcb7f9f62a8b4392972606780af7a19c62
Koykis <koykis@gmail.com>
2013-07-23 15:07:41 +0300

    Basic footer file
    Includes the wrap of the html elements and terminates the Database connection. Also has links for the terms of use and FAQ page.

---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+
+<div id="terms" class="fl"><p><a href="?page=terms" target="_blank">Terms of use</a> - <a href="?page=faq" target="_blank">F.A.Q.</a></p></div>
+<?php $conn=null; ?>
+</body>
+
+</html>
